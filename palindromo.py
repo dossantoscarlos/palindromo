31a0c8f594d097877adc202bcd0519227e7f6015
Carlos Eduardo <anjoazrael10@gmail.com>
2023-10-03 03:46:23 +0000

    corrigindo erro ao identificar frase

--- palindromo.py
+++ palindromo.py
@@ -1,38 +1,22 @@
 import unicodedata
 
-def verifica_palindromo(texto_processado, texto_original) -> None:
-    texto_original = texto_original.strip().split()
-    palavras = texto_processado.strip().split()
-    for texto_original, palavras in zip(texto_original, palavras):
-        print(message(palavras, texto_original))
+def remove_acentuacao(texto_original):
+    texto_processado = unicodedata.normalize('NFKD', texto_original).encode('ASCII', 'ignore').decode('utf-8')
+    return texto_processado
 
-def message(palavras , texto_original) -> str:
-    if palavras == palavras[::-1]:
-        return f'{texto_original} : é palíndromo'
-    return f'{texto_original} : não é palíndromo'
-
-def remove_unicode(texto) -> str: 
-    processamento = unicodedata.normalize("NFD",texto)
-    processamento = processamento.encode('ascii', 'ignore')
-    processamento = processamento.decode('utf-8')
-    return processamento
-
-def valida_exit() -> bool:
-    option = str(input('deseja continuar? digite S ou N: ')) 
-    if option.lower().strip() != "s" and option.lower().strip() != "n" :
-       return valida_exit()
-    return finaliza_program(option)
-   
-def finaliza_program (option) -> bool:
-    return option.lower().strip() == "n" 
-
-def execute () -> None:
-    while True:
-        texto_original = str(input("Digite uma frase ou palavra/numero: "))
-        verifica_palindromo(remove_unicode(texto_original), texto_original)
-        if valida_exit() == True : break
+def verifica_palindromo(texto_processado):
+    frase = remove_acentuacao(texto_processado).replace(" ", "").lower()
+    return frase == frase[::-1]
     
+if __name__=='__main__':
+    
+    input_frase = input("Digite uma frase: ").strip()
+    
+    if (len(input_frase) < 3):
+        print("O texto deve ter mais de 3 caracteres")
+        exit(1)
 
-if __name__ == '__main__':
-    execute()   
-        
+    if verifica_palindromo(input_frase):
+        print("A frase é um palíndromo")
+    else:
+        print("A frase não é um palíndromo")
