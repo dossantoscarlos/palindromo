fba301dcdd3585a1bca789db424003c08e3bcc83
Carlos Eduardo <anjoazrael10@gmail.com>
2023-10-06 00:22:57 -0300

    Update palindromo.py

--- palindromo.py
+++ palindromo.py
@@ -7,16 +7,16 @@ def remove_acentuacao(texto_original):
 def verifica_palindromo(texto_processado):
     frase = remove_acentuacao(texto_processado).replace(" ", "").lower()
     return frase == frase[::-1]
-    
-if __name__=='__main__':
-    
-    input_frase = input("Digite uma frase: ").strip()
-    
-    if (len(input_frase) < 3):
-        print("O texto deve ter mais de 3 caracteres")
-        exit(1)
 
+
+def execute() -> str:    
+    input_frase = input("Digite uma frase: ").strip()
+    if (len(input_frase) < 3):
+        return "O texto deve ter mais de 3 caracteres"
     if verifica_palindromo(input_frase):
-        print("A frase é um palíndromo")
-    else:
-        print("A frase não é um palíndromo")
+        return "A frase é um palíndromo"
+    return "A frase não é um palíndromo"
+
+if __name__=='__main__':
+    exec = execute()
+    print(f"{exec}")
